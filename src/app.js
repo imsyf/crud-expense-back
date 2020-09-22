521b64df26fa685a410e59f8c8c6f90b7cfa97a4
imsyf <alghozalyi@gmail.com>
2020-09-22 08:48:08 +0700

    Automatically create local storage bucket
    Automatically create local storage bucket (if it doesn't exist) according to .env configuration. Postfix '-bucket' added to the directory name so it can be easily ignored by git.

--- src/app.js
+++ src/app.js
@@ -19,14 +19,24 @@ app.use(cors());
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') {
-  global.localStorageBucket = path.resolve(`./${process.env.BUCKET_NAME}`);
+  const { initBucket } = require('./util/image-uploader/fs');
 
-  app.use(
-    `/${process.env.BUCKET_NAME}`,
-    express.static(global.localStorageBucket)
-  );
+  try {
+    const bucket = path.resolve(`./${process.env.BUCKET_NAME}-bucket`);
+    
+    initBucket(bucket);
+    
+    global.localStorageBucket = bucket;
 
-  console.log(`Local storage bucket base URL: http://localhost:${global.port}/${process.env.BUCKET_NAME}`);
+    app.use(
+      `/${process.env.BUCKET_NAME}-bucket`,
+      express.static(global.localStorageBucket)
+    );
+
+    console.log(`Local storage bucket base URL: http://localhost:${global.port}/${process.env.BUCKET_NAME}-bucket`);
+  } catch (error) {
+    console.error(`Failed to create local storage bucket: ${error.message}`);
+  }
 }
 app.use('/', route);
 
--- src/util/image-uploader/fs.js
+++ src/util/image-uploader/fs.js
@@ -6,7 +6,7 @@ const _upload = (stream, filename) => new Promise((resolve, reject) => {
       reject(error);
     })
     .on('finish', () => {
-      resolve(`http://localhost:${global.port}/${process.env.BUCKET_NAME}/${filename}`);
+      resolve(`http://localhost:${global.port}/${process.env.BUCKET_NAME}-bucket/${filename}`);
     });
 });
 
@@ -14,7 +14,17 @@ const _delete = (filename) => {
   fs.unlinkSync(`${global.localStorageBucket}/${filename}`);
 }
 
+const initBucket = (dir) => {
+  if (!fs.existsSync(dir)) {
+    console.log('Local storage bucket doesn\'t exist, creating one');
+    fs.mkdirSync(dir);
+  } else {
+    console.log('Use existing local storage bucket');
+  }
+};
+
 module.exports = {
   _upload,
-  _delete
+  _delete,
+  initBucket
 };
