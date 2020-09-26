79dc63839057c224ca160908fed7ee9b891d3b1e
imsyf <alghozalyi@gmail.com>
2020-09-26 14:52:42 +0700

    Change handleUpload middleware to a regular function

--- src/util/image-uploader/index.js
+++ src/util/image-uploader/index.js
@@ -1,23 +1,16 @@
 const { default: formatDate } = require('@bitty/format-date');
 const { _upload, _delete } = process.env.NODE_ENV === 'production' ? require('./gcs') : require('./fs');
 
-const handleUpload = async (req, res, next) => {
-  if (req.file) {
-    try {
-      if (!req.file.detectedFileExtension.match(/^(.jpg|.png|.gif)$/)) {
-        res.status(400);
-        throw new Error(`⛔ - Unsupported filetype - Pick .jpg, .png, or .gif instead of ${req.file.detectedFileExtension || req.file.clientReportedFileExtension}`);
-      }
-
-      const targetFileName = `${formatDate(new Date(), 'YYYYMMDD-HHmmss')}-${Math.round(Math.random() * 1E9)}${req.file.detectedFileExtension}`;
-
-      req.file.publicUrl = await _upload(req.file.stream, targetFileName);
-    } catch (error) {
-      next(error);
-    }
+const handleUpload = async (file, res) => {
+  if (!file.detectedFileExtension.match(/^(.jpg|.png|.gif)$/)) {
+    res.status(400);
+    throw new Error(`📷 Only .jpg, .png, or .gif image file is allowed, but you provided ${file.detectedFileExtension || file.clientReportedFileExtension} instead`);
   }
-  return next();
-};
+
+  const targetFileName = `${formatDate(new Date(), 'YYYYMMDD-HHmmss')}-${Math.round(Math.random() * 1E9)}${file.detectedFileExtension}`;
+
+  return await _upload(file.stream, targetFileName);
+}
 
 const handleDelete = async (publicUrl) => {
   const segments = publicUrl.split('/');
