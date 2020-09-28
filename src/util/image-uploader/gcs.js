aedb10ea58937b355f7b5049f1b77856105d02c2
imsyf <alghozalyi@gmail.com>
2020-09-28 19:32:09 +0700

    GCS object uploader and delete-r(?) implementation details

--- src/util/image-uploader/gcs.js
+++ src/util/image-uploader/gcs.js
@@ -1,10 +1,37 @@
-const _upload = () => {
-  console.log('Upload image to Google Cloud Storage bucket.');
-};
+const { Storage } = require('@google-cloud/storage');
+const path = require('path');
 
-const _delete = () => {
-  console.log('Delete image from Google Cloud Storage bucket.');
-}
+const storage = new Storage({
+  keyFilename: path.join(__dirname, './service-key.json'),
+  projectId: process.env.GCP_PROJECT_ID
+});
+
+const bucket = storage.bucket(process.env.BUCKET_NAME);
+
+const _upload = (stream, filename) => new Promise((resolve, reject) => {
+  stream.pipe(
+    bucket.file(filename)
+      .createWriteStream({
+        metadata: {
+          cacheControl: 'max-age=0, no-cache'
+        }
+      })
+  )
+    .on('error', (error) => {
+      reject(error);
+    })
+    .on('finish', () => {
+      resolve(`https://storage.googleapis.com/${process.env.BUCKET_NAME}/${filename}`);
+    });
+});
+
+const _delete = (filename) => new Promise((resolve, reject) => {
+  bucket.file(filename)
+    .delete((error, response) => {
+      if (error) reject(error);
+      resolve(response);
+    });
+});
 
 module.exports = {
   _upload,
